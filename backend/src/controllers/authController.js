a6fd81b2ab6335dbb851a246fd8a97c0cfd5dbfb
madebytime <matheus.raws@gmail.com>
2019-10-29 06:11:02 -0300

    trabalhando na autenticação

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -6,28 +6,27 @@ const User = require('../models/User');
 
 const router = express.Router();
 
-function generateToken(params = {}) {
-    return jwt.sign({ _id: user._id }, authConfig.secret, {
-        expiresIn: 86400,
+function generateToken(params = {}){
+    return  jwt.sign(params, authConfig.secret,{
+        expiresIn:86400,
     });
-
 }
-
-
 router.post('/register', async (req, res) => {
     const { cpf, email } = req.body;
     try {
         if (await User.findOne({ cpf, email }))
-            return res.status(400).send({ error: 'Esse campo já possui usuário registrado' })
+            return res.status(400).send({ error: 'Usuário já existe' });
 
         const user = await User.create(req.body);
         user.password = undefined;
 
-        return res.send({ user ,token:generateToken({_id: user._id})});
+        return res.send({ user, token: generateToken({_id: user._id}) });
+
     } catch (err) {
-        res.status(400).send({ error: 'Não foi possivel' });
+        return res.status(400).send({ error: ' Registration Failed' });
 
     }
+
 });
 
 router.post('/authenticate', async (req, res) => {
@@ -42,11 +41,7 @@ router.post('/authenticate', async (req, res) => {
 
     user.password = undefined;
 
-    const token = jwt.sign({ _id: user._id }, authConfig.secret, {
-        expiresIn: 86400,
-    });
-
-    res.send({ user, token: generateToken({ _id: user._id }) });
+    res.send({ user,token: generateToken({_id: user._id}) });
 
 });
 
