149589025b986523ba9d7a4a2b5ed2447502a52f
matheus.raws@gmail.com <matheus.raws@gmail.com>
2019-11-01 09:16:39 -0300

    Definido as pastas do projeto

--- GenServ/screens/HomeScreen.js
+++ GenServ/screens/HomeScreen.js
@@ -29,33 +29,12 @@ export default function HomeScreen() {
           />
         </View>
 
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Hello</Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
+     
       </ScrollView>
 
       <View style={styles.tabBarInfoContainer}>
         <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
+          Bora MARAGOGIPE KLAN!!
         </Text>
 
         <View
---
+++ GenServ/screens/LoginPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Login() {
+    return
+        <View>
+
+            <Text>Hello World</Text>
+        </View>
+    
+}
