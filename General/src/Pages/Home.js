c531110e071643a1ae252a76573f48ff58a97402
madebytime <matheus.raws@gmail.com>
2019-11-02 21:32:31 -0300

    criada pagina HOME e CADASTRO

---
+++ General/src/Pages/Home.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import Lottie from 'lottie-react-native';
+
+import services from '../Animations/services.json'
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <View style={{ flex:1, justifyContent: 'center', alignItems: 'center', marginTop: 150}}>
+                <Lottie resizeMode='contain' autoSize source={services} autoPlay loop />
+            </View>
+            <View style={styles.bottom}>
+                <TouchableOpacity style={styles.button1}>
+                    <Text style={styles.buttonText}>Logar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button2}>
+                    <Text style={styles.buttonText}>Cadastre-se</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+
+    bottom: {
+        flex: 1,
+        flexDirection:'row',
+        justifyContent: 'space-around',
+        alignItems:'flex-end',
+        marginBottom: 35,
+        paddingHorizontal: 35,
+        
+    },
+    button1: {
+        borderColor:'#000',
+        borderWidth:1,
+        height: 50,
+        backgroundColor: '#FFF',
+        borderRadius: 7,
+        width:120,
+        marginRight:30,
+        alignItems:'center',
+        justifyContent:'center', 
+
+    },
+
+    buttonText: {
+        fontSize: 20,
+        fontWeight:'bold',
+        color:'#000',       
+
+    },
+
+    button2: {
+        borderColor:'#000',
+        borderWidth:1,
+        height: 50,
+        backgroundColor: '#FFF',
+        borderRadius: 7,
+        width:120,
+        marginLeft:30,
+        marginRight:10,
+        alignItems:'center',
+        justifyContent:'center',
+
+    }
+
+
+});
---
+++ General/src/Pages/Login.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, KeyboardAvoidingView} from 'react-native';
+
+export default function Login() {
+    return (
+        <KeyboardAvoidingView behavior='padding'  style={styles.container}>
+            <View style={styles.form}>
+                <Text>Nome completo*</Text>
+                <TextInput style={styles.input} autoCapitalize="words" />
+                <Text>E-mail*</Text>
+                <TextInput style={styles.input} keyboardType='email-address'
+                    autoCapitalize="none" autoCorrect={false} />
+                <Text>CPF*</Text>
+                <TextInput style={styles.input} keyboardType='numeric' />
+                <Text>Data de Nascimeto</Text>
+                <TextInput style={styles.input} keyboardType='default' />
+                <Text>Senha</Text>
+                <TextInput style={styles.input} />
+                <Text>Confirmar Senha</Text>
+                <TextInput style={styles.input} />
+
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}> Cadastrar</Text>
+                </TouchableOpacity>
+            </View>
+
+        </KeyboardAvoidingView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    form: {
+        alignSelf: 'stretch',
+        paddingHorizontal: 30,
+        marginTop: 20,
+    },
+    text: {
+        fontWeight: 'bold',
+        color: '#444',
+        marginBottom: 8,
+    },
+    input: {
+        borderWidth: 2,
+        borderColor: '#ddd',
+        paddingHorizontal: 20,
+        fontSize: 16,
+        color: '#444',
+        height: 44,
+        marginBottom: 20,
+        borderRadius: 3,
+
+    },
+    button: {
+        height: 42,
+        backgroundColor: '#000',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 4,
+    },
+    buttonText: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 16,
+
+    }
+})
+
