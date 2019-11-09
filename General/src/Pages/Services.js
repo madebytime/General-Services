2c5dd4f2700daf84168a62187f65f616531ebad5
Gabriel-17 <gabrielr440@gmail.com>
2019-11-08 22:39:10 -0300

    Teste

--- General/src/Pages/Services.js
+++ General/src/Pages/Services.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View,
     Text,
     StyleSheet,
+    Image,ScrollView,
     container } from 'react-native';
 
 // import { Container } from './styles';
@@ -9,11 +10,21 @@ import { View,
 class Services extends Component {
   render (){
     return (
-      <View style={styles.container}>
-        <Text>
-          Olá Services
-        </Text>
-      </View>
+      <ScrollView>
+       <View style={styles.container}>
+         <Text style={styles.Title}>
+          General Services
+         </Text>
+       </View>
+                <View style={styles.container}>
+                    <Image style={{ width: 150, height: 150 }}  source={require('../Images/Mecanico.jpg')} />
+                    <Text>Procurar Mecânicos</Text>
+                </View>
+                <View style={styles.container}>
+                    <Image style={{ width: 150, height: 150 }}  source={require('../Images/Delivery.png')} />
+                    <Text>Serviços Busca e entregas</Text>
+                </View>
+            </ScrollView>
     );
   }
 }
@@ -26,5 +37,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#fff',
+  },
+  Title: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 30,
+    marginTop: 30,
+    alignSelf: 'center',
   }
 });
