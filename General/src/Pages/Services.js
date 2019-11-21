2a62aebb12f6ea51e9caf2dfac00a2fd33ca3979
Gabriel-17 <gabrielr440@gmail.com>
2019-11-21 09:26:32 -0300

    Pagina serviço, add Icones

--- General/src/Pages/Services.js
+++ General/src/Pages/Services.js
@@ -1,30 +1,75 @@
 import React, { Component } from 'react';
-import { View,
-    Text,
-    StyleSheet,
-    Image,ScrollView,
-    container } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image, ScrollView, TouchableOpacity,
+  container
+} from 'react-native';
 
 // import { Container } from './styles';
 
 class Services extends Component {
-  render (){
+
+  render() {
     return (
       <ScrollView>
-       <View style={styles.container}>
-         <Text style={styles.Title}>
-          General Services
+        <View style={styles.Title}>
+          <Text>
+            Hōshi
          </Text>
-       </View>
-                <View style={styles.container}>
-                    <Image style={{ width: 150, height: 150 }}  source={require('../Images/Mecanico.jpg')} />
-                    <Text>Procurar Mecânicos</Text>
-                </View>
-                <View style={styles.container}>
-                    <Image style={{ width: 150, height: 150 }}  source={require('../Images/Delivery.png')} />
-                    <Text>Serviços Busca e entregas</Text>
-                </View>
-            </ScrollView>
+        </View>
+        <View style={styles.container} >
+          <View style={styles.iconPadron}>
+            <TouchableOpacity style={styles.Icons}>
+              <Image style={{ width: 70, height: 70 }} source={require('../Images/mechanic.png')} />
+              <Text>Mecânicos</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.Icons}>
+              <Image style={{ width: 70, height: 70 }} source={require('../Images/plumber.png')} />
+              <Text>Encanador</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.iconPadron}>
+            <TouchableOpacity>
+              <Image style={{ width: 70, height: 70 }} source={require('../Images/makeup.png')} />
+              <Text>Makeup</Text>
+            </TouchableOpacity><TouchableOpacity>
+              <Image style={{ width: 70, height: 70 }} source={require('../Images/electrician.png')} />
+              <Text>Eletricitas</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.iconPadron}>
+            <TouchableOpacity>
+              <Image style={{ width: 70, height: 70 }} source={require('../Images/hairstylist.png')} />
+              <Text>Cabeleleira</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image style={{ width: 70, height: 70 }} source={require('../Images/photographer.png')} />
+              <Text>Fotógrafos</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.iconPadron}>
+            <TouchableOpacity>
+              <Image style={{ width: 70, height: 70 }} source={require('../Images/brickwall.png')} />
+              <Text>Confeiteros</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image style={{ width: 70, height: 70 }} source={require('../Images/waiters.png')} />
+              <Text>Garçons</Text>
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity>
+            <Image style={{ width: 70, height: 70 }} source={require('../Images/Delivery.png')} />
+            <Text>Entregas</Text>
+
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image style={{ width: 70, height: 70 }} source={require('../Images/bartender.png')} />
+            <Text>Barman</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     );
   }
 }
@@ -33,9 +78,8 @@ export default Services;
 
 const styles = StyleSheet.create({
   container: {
+    margin: 25,
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#fff',
   },
   Title: {
@@ -44,5 +88,18 @@ const styles = StyleSheet.create({
     fontSize: 30,
     marginTop: 30,
     alignSelf: 'center',
+  },
+  iconPadron: {
+    flex: 1,
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  Icons: {
+    margin: 7,
+    borderRadius: 10,
+
   }
 });
